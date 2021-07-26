fa13f4aad5ce37d51cf58375ffe158324defdca8
skaerf <lawrence.harrison@outlook.com>
2021-07-26 22:30:23 +0100

    actionbar + join title

---
+++ src/main/java/net/skaerf/ksmp/ActionBar.java
@@ -0,0 +1,103 @@
+package net.skaerf.ksmp;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class ActionBar {
+
+    private static Plugin plugin;
+    private static String nmsversion;
+    private static boolean useOldMethods;
+
+    public static void enableThings() {
+        plugin = Main.getPlugin(Main.class);
+        nmsversion = Bukkit.getServer().getClass().getPackage().getName();
+        nmsversion = nmsversion.substring(nmsversion.lastIndexOf(".") + 1);
+        if (nmsversion.equalsIgnoreCase("v1_8_R1") || nmsversion.startsWith("v1_7_")) { // Not sure if 1_7 works for the protocol hack?
+            useOldMethods = true;
+        }
+    }
+
+    public static void sendActionBar(Player player, String message) {
+        if (!player.isOnline()) {
+            return;
+        }
+        try {
+            Class<?> craftPlayerClass = Class.forName("org.bukkit.craftbukkit." + nmsversion + ".entity.CraftPlayer");
+            Object craftPlayer = craftPlayerClass.cast(player);
+            Object packet;
+            Class<?> packetPlayOutChatClass = Class.forName("net.minecraft.server." + nmsversion + ".PacketPlayOutChat");
+            Class<?> packetClass = Class.forName("net.minecraft.server." + nmsversion + ".Packet");
+            if (useOldMethods) {
+                Class<?> chatSerializerClass = Class.forName("net.minecraft.server." + nmsversion + ".ChatSerializer");
+                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsversion + ".IChatBaseComponent");
+                Method m3 = chatSerializerClass.getDeclaredMethod("a", String.class);
+                Object cbc = iChatBaseComponentClass.cast(m3.invoke(chatSerializerClass, "{\"text\": \"" + message + "\"}"));
+                packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(cbc, (byte) 2);
+            } else {
+                Class<?> chatComponentTextClass = Class.forName("net.minecraft.server." + nmsversion + ".ChatComponentText");
+                Class<?> iChatBaseComponentClass = Class.forName("net.minecraft.server." + nmsversion + ".IChatBaseComponent");
+                try {
+                    Class<?> chatMessageTypeClass = Class.forName("net.minecraft.server." + nmsversion + ".ChatMessageType");
+                    Object[] chatMessageTypes = chatMessageTypeClass.getEnumConstants();
+                    Object chatMessageType = null;
+                    for (Object obj : chatMessageTypes) {
+                        if (obj.toString().equals("GAME_INFO")) {
+                            chatMessageType = obj;
+                        }
+                    }
+                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, chatMessageTypeClass}).newInstance(chatCompontentText, chatMessageType);
+                } catch (ClassNotFoundException cnfe) {
+                    Object chatCompontentText = chatComponentTextClass.getConstructor(new Class<?>[]{String.class}).newInstance(message);
+                    packet = packetPlayOutChatClass.getConstructor(new Class<?>[]{iChatBaseComponentClass, byte.class}).newInstance(chatCompontentText, (byte) 2);
+                }
+            }
+            Method craftPlayerHandleMethod = craftPlayerClass.getDeclaredMethod("getHandle");
+            Object craftPlayerHandle = craftPlayerHandleMethod.invoke(craftPlayer);
+            Field playerConnectionField = craftPlayerHandle.getClass().getDeclaredField("playerConnection");
+            Object playerConnection = playerConnectionField.get(craftPlayerHandle);
+            Method sendPacketMethod = playerConnection.getClass().getDeclaredMethod("sendPacket", packetClass);
+            sendPacketMethod.invoke(playerConnection, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void sendActionBar(final Player player, final String message, int duration) {
+        sendActionBar(player, message);
+
+        if (duration >= 0) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, "");
+                }
+            }.runTaskLater(plugin, duration + 1);
+        }
+        while (duration > 40) {
+            duration -= 40;
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    sendActionBar(player, message);
+                }
+            }.runTaskLater(plugin, (long) duration);
+        }
+    }
+
+    public static void sendActionBarToAllPlayers(String message) {
+        sendActionBarToAllPlayers(message, -1);
+    }
+
+    public static void sendActionBarToAllPlayers(String message, int duration) {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            sendActionBar(p, message, duration);
+        }
+    }
+}
--- src/main/java/net/skaerf/ksmp/Main.java
+++ src/main/java/net/skaerf/ksmp/Main.java
@@ -1,10 +1,36 @@
 package net.skaerf.ksmp;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin implements Listener {
 
     public void onEnable() {
+        runnable();
+        ActionBar.enableThings();
+        getServer().getPluginManager().registerEvents(this, this);
         System.out.println("[KSMP] Base plugin initialising");
     }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendTitle(ChatColor.translateAlternateColorCodes('&', "&7Welcome to &bKSMP"), ChatColor.translateAlternateColorCodes('&', "&6Season Eight &7- &fsponsored by &3riplhost"), 10, 50, 10);
+    }
+
+    public void runnable() {
+        new BukkitRunnable() {
+
+            @Override
+            public void run() {
+                ActionBar.sendActionBarToAllPlayers(ChatColor.translateAlternateColorCodes('&', "&6You are playing on KSMP &7- &fsponsored by &3riplhost"), 10);
+            }
+        }.runTaskLaterAsynchronously(this, 60000);
+    }
+
 }
