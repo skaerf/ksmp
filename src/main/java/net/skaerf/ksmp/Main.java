92f614a89eb1dc101247cd51a69e6b65e1e2af1e
skaerf <lawrence.harrison@outlook.com>
2021-07-26 21:53:13 +0100

    initial commit

---
+++ src/main/java/net/skaerf/ksmp/Main.java
@@ -0,0 +1,10 @@
+package net.skaerf.ksmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+        System.out.println("[KSMP] Base plugin initialising");
+    }
+}
